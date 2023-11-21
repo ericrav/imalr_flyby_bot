6b63121868c9e87f63a9910716bb7f5a9234b73d
Eric Rabinowitz <ericrabinowitz@me.com>
2023-11-21 12:45:47 -0500

    make vortex sketch

--- sketches/sketch.js
+++ sketches/sketch.js
@@ -5,14 +5,24 @@ new p5();
 
 const settings = {
   p5: true,
-  dimensions: [400, 400],
+  dimensions: [800, 800],
 };
 
 const sketch = () => {
-  return () => {
-    background(255, 0, 0);
-    circle(width / 2, height / 2, 50);
-  };
+  background(255);
+  noFill();
+  let s = 255;
+  translate(width / 2, height / 2);
+
+  for (let i = 0; i < 800; i++) {
+    stroke(color(s, s, s, 5 + (Math.random() * 5) | 0));
+    strokeWeight(1 + Math.random() * 5);
+    rect(-25, -25, 50, 50);
+    scale(1.003125 + Math.random() * 0.003125);
+    rotate(0.025 + Math.random() * 0.025);
+    s = Math.max(0, Math.min(255, s - (Math.random() * 3 - 1) | 0));
+  }
+
 };
 
 canvasSketch(sketch, settings);
